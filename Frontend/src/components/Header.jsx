8d15533a1ef3bf12d63d103f1eeb0f2df8e0fd7d
Rohan-devv <devv112004@gmail.com>
2025-08-06 22:58:48 +0530

    ....

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h3> MERN BLOG APP </h3>
+            <ul>
+                <li>Home </li>
+                <li>Add Blog</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
