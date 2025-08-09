49e6ff4372966df568f784a9d19f256ce733d731
Rohan-devv <devv112004@gmail.com>
2025-08-09 23:45:06 +0530

    jdijkdnk

--- Frontend/src/pages/AddBlog.jsx
+++ Frontend/src/pages/AddBlog.jsx
@@ -57,9 +57,10 @@ export default function AddBlog() {
 
 
 <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+  <h1 className="text-3xl font-bold text-center text-gray-800 mb-6"></h1>
 
 
-
+<h1 className="text-3xl font-bold text-center text-gray-800 mb-6"></h1>
 
 
 
