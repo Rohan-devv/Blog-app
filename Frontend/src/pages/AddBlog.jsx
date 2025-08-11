acac3be87ccfeb706c72560101b7f099a27ffff2
Rohan-devv <devv112004@gmail.com>
2025-08-11 21:48:26 +0530

    jbfii

--- Frontend/src/pages/AddBlog.jsx
+++ Frontend/src/pages/AddBlog.jsx
@@ -10,6 +10,7 @@ export default function AddBlog() {
   const location = useLocation();
 
   <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+   <h1 className="text-3xl font-bold text-center text-gray-800 mb-6"></h1> 
 
 
 
