0485a30c117517fd495b9050fe3d02a34a6de1a7
Rohan-devv <devv112004@gmail.com>
2025-08-13 16:22:22 +0530

    nedk

--- Frontend/src/pages/AddBlog.jsx
+++ Frontend/src/pages/AddBlog.jsx
@@ -9,11 +9,7 @@ export default function AddBlog() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
-   <h1 className="text-3xl font-bold text-center text-gray-800 mb-6"></h1> 
-   <h1 className="text-3xl font-bold text-center text-gray-800 mb-6"></h1>
-
-
+ <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
 
   console.log(formData);
 
