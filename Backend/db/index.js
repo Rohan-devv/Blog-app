80a664055478571246dfaaa540d8b3c102e9e9fc
Rohan-devv <devv112004@gmail.com>
2025-08-20 02:23:32 +0530

    perfect its done

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.set('strictQuery', false);
-mongoose.connect("process.env.MONGO_URI")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.error("Could not connect to MongoDB...", err));
 
