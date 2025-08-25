ea406f34a2457f378b62dc341965c7e9e13a3da4
Rohan-devv <devv112004@gmail.com>
2025-08-26 03:55:59 +0530

    wedfrghj

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,7 @@ const blogRouter = require('./routes/blog-route');
 require('dotenv').config();
 
 const app = express();
-app.use(cors({
-  origin: ["https://blog-app-git-main-rohan-pals-projects.vercel.app"],  // ✅ allow your Vercel site
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogRouter);
